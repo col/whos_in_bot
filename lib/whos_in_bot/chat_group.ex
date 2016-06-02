f52d7afbe99fa201b00e7d98f4f924b15b039187
Col Harris <col.w.harris@gmail.com>
2016-06-02 23:00:13 +0800

    switch chat group processes to use local process registration

--- lib/whos_in_bot/chat_group.ex
+++ lib/whos_in_bot/chat_group.ex
@@ -6,22 +6,13 @@ defmodule WhosInBot.ChatGroup do
     GenServer.start_link(__MODULE__, chat_id)
   end
 
-  defp chat_group_process(chat_id) do
-    case :global.whereis_name({:chat_group, chat_id}) do
-      :undefined ->
-        {:ok, pid} = WhosInBot.ChatGroupSupervisor.start_chat_group(chat_id)
-        pid
-      pid -> pid
-    end
-  end
-
   def handle_message(message) do
     GenServer.cast(chat_group_process(message.chat.id), {:handle_message, message})
   end
 
   def init(chat_id) do
     IO.puts "Starting new Chat Group: #{chat_id}"
-    :global.register_name({:chat_group, chat_id}, self)
+    register(chat_id)
     {:ok, nil}
   end
 
@@ -34,6 +25,28 @@ defmodule WhosInBot.ChatGroup do
         IO.puts "Error handling message: #{response}"
     end
     {:noreply, roll_call}
+  end
+
+
+  defp chat_group_process(chat_id) do
+    case where_is(chat_id) do
+      :undefined ->
+        {:ok, pid} = WhosInBot.ChatGroupSupervisor.start_chat_group(chat_id)
+        pid
+      pid -> pid
+    end
+  end
+
+  defp where_is(chat_id) do
+    Process.whereis(process_name(chat_id))
+  end
+
+  defp register(chat_id) do
+    Process.register(self, process_name(chat_id))
+  end
+
+  defp process_name(chat_id) do
+    String.to_atom("chat_id:#{chat_id}")
   end
 
 end
