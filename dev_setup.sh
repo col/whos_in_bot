4228de05e4d453a5242c6339b34c8272074cc6e0
Colin Harris <col.w.harris@gmail.com>
2018-09-10 09:39:09 +0800

    Update the dev setup script

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Start dependent services (prostgres)
-docker-compose up -d
+docker-compose up -d postgres
 
 # Wait for postgres to be available`
 until docker-compose exec postgres psql -U "postgres" -c '\q'; do
@@ -10,23 +10,15 @@ until docker-compose exec postgres psql -U "postgres" -c '\q'; do
 done
 >&2 echo "Postgres is up - executing command"
 
-# Create dev and test databases
-for db_name in "whos_in_bot_dev" "whos_in_bot_test"; do
-    if docker-compose exec postgres psql -Upostgres -lqt | cut -d \| -f 1 | grep -qw $db_name; then
-        echo "Database $db_name already exists"
-    else
-        echo "Database $db_name does not exist"
-        echo "Creating ..."
-        docker-compose exec postgres psql -Upostgres -c "create database $db_name"
-        echo "Done"
-    fi
-done
-
 # Get elixir dependencies
 mix deps.get
 
-# Setup db
-mix ecto.create && mix ecto.migrate
+# Create DB
+mix ecto.create
+MIX_ENV=test mix ecto.create
+
+# Migrate DB
+mix ecto.migrate
 
 # Run tests
 mix test
