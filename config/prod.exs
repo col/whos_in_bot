cbb5edf169b474f0c08c326eaa5fc2111172b641
Colin Harris <col.w.harris@gmail.com>
2019-08-14 00:08:20 +0800

    Provide default DB credentials in prod

--- config/prod.exs
+++ config/prod.exs
@@ -4,11 +4,11 @@ config :logger, level: :warn
 
 config :whos_in_bot,
        WhosInBot.Repo,
-       username: System.fetch_env!("DB_USER"),
-       password: System.fetch_env!("DB_PASSWORD"),
-       database: System.fetch_env!("DB_NAME"),
-       hostname: System.fetch_env!("DB_HOST"),
-       port: System.fetch_env!("DB_PORT") |> String.to_integer(),
+       username: System.get_env("DB_USER", "postgres"),
+       password: System.get_env("DB_PASSWORD", "postgres"),
+       database: System.get_env("DB_NAME", "whos_in_bot"),
+       hostname: System.get_env("DB_HOST", "localhost"),
+       port: System.get_env("DB_PORT", "5432") |> String.to_integer(),
        ssl: System.get_env("DB_SSL", "false") |> String.to_existing_atom()
 
 if File.exists?("prod.secret.exs") do
